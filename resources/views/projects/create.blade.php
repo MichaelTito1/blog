e9ba36ea31661ce137d4526548ff30d2867f34ce
MICHAEL TITO <maicheltito1@gmail.com>
2022-03-06 16:41:51 +0200

    created views for CRUD operations

---
+++ resources/views/projects/create.blade.php
@@ -0,0 +1,66 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="row">
+        <div class="col-lg-12 margin-tb">
+            <div class="pull-left">
+                <h2>Add New Product</h2>
+            </div>
+            <div class="pull-right">
+                <a class="btn btn-primary" href="{{ route('projects.index') }}" title="Go back"> <i class="fas fa-backward "></i> </a>
+            </div>
+        </div>
+    </div>
+
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <strong>Whoops!</strong> There were some problems with your input.<br><br>
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+    <form action="{{ route('projects.store') }}" method="POST" >
+        @csrf
+
+        <div class="row">
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>month:</strong>
+                    <input type="text" name="month" class="form-control" placeholder="month">
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>bonus payment day:</strong>
+                    <textarea class="form-control" style="height:50px" name="bonus_payment_day"
+                        placeholder="bonus_payment_day"></textarea>
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>Salaries Total:</strong>
+                    <input type="text" name="salaries_total" class="form-control" placeholder="salaries_total">
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>Bonus Total:</strong>
+                    <input type="number" name="bonus_total" class="form-control" placeholder="bonus_total">
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>Payments Total:</strong>
+                    <input type="number" name="payments_total" class="form-control" placeholder="payments_total">
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-12 text-center">
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </div>
+        </div>
+
+    </form>
+@endsection
---
+++ resources/views/projects/show.blade.php
@@ -0,0 +1,54 @@
+@extends('layouts.app')
+
+
+@section('content')
+    <div class="row">
+        <div class="col-lg-12 margin-tb">
+            <div class="pull-left">
+                <h2>  {{ $project->name }}</h2>
+            </div>
+            <div class="pull-right">
+                <a class="btn btn-primary" href="{{ route('projects.index') }}" title="Go back"> <i class="fas fa-backward "></i> </a>
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>month:</strong>
+                {{ $project->month }}
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>salaries_payment_day:</strong>
+                {{ $project->salaries_payment_day }}
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>bonus_payment_day:</strong>
+                {{ $project->bonus_payment_day }}
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>salaries_total:</strong>
+                {{ $project->salaries_total }}
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>bonus_total:</strong>
+                {{ $project->bonus_total }}
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>payments_total:</strong>
+                {{ $project->payments_total }}
+            </div>
+        </div>
+    </div>
+@endsection
